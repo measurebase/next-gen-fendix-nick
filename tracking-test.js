8d721e42e29863ca5cc4433057f309854afe0814
measurebase <info@nickvanmaaren.nl>
2026-02-26 10:08:17 +0100

    Update tracking-test.js

--- tracking-test.js
+++ tracking-test.js
@@ -7,53 +7,52 @@ document.querySelectorAll('form').forEach((form, i) => {
     
     // Form start
     form.addEventListener('focusin', () => {
-      if (started) return;
-      started = true;
-      
-      dataLayer.push('form_start', {
-        form_id: formId,
-        form_name: formName,
-        page_path: page.path
-      });
+        if (started) return;
+            started = true;
+            
+            dataLayer.push('form_start', {
+            form_id: formId,
+            form_name: formName,
+            page_path: page.path
+        });
     }, { once: true });
     
     // Form submit
     form.addEventListener('submit', () => {
-      // Check of dit eerste submit is
-      const submitted = storage.get('forms_submitted') || [];
-      const isFirst = !submitted.includes(formId);
-      
-      if (isFirst) {
-        submitted.push(formId);
-        storage.set('forms_submitted', submitted);
-      }
-      
-      dataLayer.push('form_submit', {
-        form_id: formId,
-        form_name: formName,
-        is_first_submit: isFirst,
-        page_path: page.path,
-        page_type: page.type
-      });
-    });
-  });
-  
-  // Webflow success message
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach((m) => {
-      m.addedNodes.forEach((node) => {
-        if (node.nodeType === 1 && node.classList?.contains('w-form-done')) {
-          dataLayer.push('form_success', {
-            page_path: page.path
-          });
+        // Check of dit eerste submit is
+        const submitted = storage.get('forms_submitted') || [];
+        const isFirst = !submitted.includes(formId);
+        
+        if (isFirst) {
+            submitted.push(formId);
+            storage.set('forms_submitted', submitted);
         }
-      });
+        
+        dataLayer.push('form_submit', {
+            form_id: formId,
+            form_name: formName,
+            is_first_submit: isFirst,
+            page_path: page.path,
+            page_type: page.type
+        });
     });
-  });
-  
-  document.querySelectorAll('.w-form').forEach(wrapper => {
+});
+    
+// Webflow success message
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((m) => {
+        m.addedNodes.forEach((node) => {
+            if (node.nodeType === 1 && node.classList?.contains('w-form-done')) {
+                dataLayer.push('form_success', {
+                    page_path: page.path
+                });
+            }
+        });
+    });
+});
+
+document.querySelectorAll('.w-form').forEach(wrapper => {
     observer.observe(wrapper, { childList: true, subtree: true });
-  });
-}
+});
 
 console.log('end_next-gen-fendix-nick_script');
